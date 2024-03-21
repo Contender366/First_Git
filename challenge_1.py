fb235776ef4b74a38d4d8a0349615797d84e6735
Contender366 <abdul366@yahoo.com>
2024-03-21 00:44:55 +0000

    first commit

---
+++ challenge_1.py
@@ -0,0 +1,16 @@
+"""Challenge 1 of Task 2"""
+
+import math
+
+# Ask the user to enter the lengths of all three sides of a triangle.
+# Calculate the area of the triangle.
+# Print out the area.
+
+a = int(input("Please enter the first side: "))
+b = int(input("Please enter the second side: "))
+c = int(input("Please enter the third side: "))
+
+s = (a + b + c) / 2
+area = math.sqrt(s*((s-a)*(s-b)*(s-c)))
+
+print(f"The area of the triangle is: {area}")
